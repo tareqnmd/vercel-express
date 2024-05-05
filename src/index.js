f56bafa343d798b8dc6eac559fb720820663ba7a
Tareq <tareqnmd@gmail.com>
2024-05-05 13:40:14 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
+//DECLARATION
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import { PORT } from './config/secret.js';
-import { connectDB } from './db.js';
+import { PORT, connectDB } from './config/index.js';
 import middleware from './middleware/index.js';
 import routes from './routes/index.js';
 
+//CONFIGURATION
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 connectDB();
 
+// MIDDLEWARE
 middleware(app, cors);
 
+//ROUTES
 routes(app);
 
+// SERVER
 app.listen(PORT, () => {
-	console.log(`Server started on port ${PORT}`);
+	console.log(`Server started on port -> ${PORT}`);
 });
 
 export default app;
