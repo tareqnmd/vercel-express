0145234d73883aac41054c943fd68dffc5905ff8
Tareq <tareqnmd@gmail.com>
2024-05-05 13:33:25 +0600

    middleware added

--- src/config/secret.js
+++ src/config/secret.js
@@ -1,5 +1,5 @@
 export const USERNAME = process.env.USERNAME;
 export const PASSWORD = process.env.PASSWORD;
-export const PORT = process.env.PORT;
+export const PORT = 3200;
 export const JWT_SECRET = 'tareq';
 export const NODE_ENV = 'development';
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
+import { PORT } from './config/secret.js';
 import { connectDB } from './db.js';
+import middleware from './middleware/index.js';
 import routes from './routes/index.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
 connectDB();
+
+middleware(app, cors);
 
 routes(app);
 
-app.listen(process.env.PORT, () => {
-	console.log(`Server started on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
 });
 
 export default app;
