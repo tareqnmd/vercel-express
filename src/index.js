4248ba5bc02b73b3a01e4667fb5d3bdf485ee0e9
Tareq <tareqnmd@gmail.com>
2024-05-05 12:18:43 +0600

    asyncHandler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
 
 const app = express();
@@ -10,6 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
+export const asyncTest = asyncHandler(async (req, res) => {
+	res.send('Hello Check!');
+});
+
 app.get('/', (req, res) => {
 	var salt = bcrypt.genSaltSync(10);
 	var hash = bcrypt.hashSync('B4c0//', salt);
@@ -17,9 +22,7 @@ app.get('/', (req, res) => {
 	res.send(`Hello Chicken cookie cors json urlencoded! ${hash} - ${token}`);
 });
 
-app.get('/check', (req, res) => {
-	res.send('Hello Check!');
-});
+app.get('/check', asyncTest);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server started on port ${process.env.PORT}`);
